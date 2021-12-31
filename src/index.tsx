62c4ad50a798206a548eb089cd8bc105955ddea9
Florian Völker <Neirolf2030@web.de>
2021-12-31 22:01:46 +0100

    updated to yarn v3

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ import ReactDOM from "react-dom";
 import './base.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<h1>Hi this is the app!</h1>, document.getElementById("root"));
+    const mountElem = document.getElementById("root");
+    ReactDOM.render(<h1>Hi this is the app!</h1>, mountElem);
 });
